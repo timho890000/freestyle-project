106f7a850a52cd24c15f343d68120f74954a829f
timho890000 <39267632+timho890000@users.noreply.github.com>
2018-06-23 10:58:05 -0400

    Refactored some functions

--- expense-tracker.py
+++ expense-tracker.py
@@ -8,7 +8,7 @@ def show_account(url):
     account_body = json.loads(account.text)
     data = (account_body["response"])
     account_info = data["accounts"][0]
-    print("Your balance in "+account_info["name"]+" is "+ str(account_info["balance"]))
+    print("Here is a summary of your spending in the account: "+account_info["name"])
     return account_info
 
 def list_transactions(url):
@@ -23,15 +23,23 @@ def include_category_key_words(category):
     included = return_list("db/"+category+".csv")
     return included
 
-
-
-
 def return_list(filename):
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         my_list = list(reader)
     return my_list[0]
 
+def include_transactions(transactions,key_words):
+    included_transactions = []
+
+    for t in transactions:
+        if key_words==["positive"] and -t["expenseAmount"]>0:
+            included_transactions.append(t)
+        elif key_words != ["other"]:
+            for f in key_words:
+                if(f.upper() in t["description"].upper()):
+                    included_transactions.append(t)
+    return included_transactions
 #def group_transactions
 
 #def matching_product(product_identifier,categories):
@@ -45,30 +53,45 @@ def run():
 
     #read the available categories (this is a list)
     categories = return_list("db/categories.csv")
-
     #retrieve the token for your account's data
     base = "https://www.buxfer.com/api";
     login_url  = base + "/login?userid=" + username + "&password=" + password
     response = requests.get(login_url)
     response_body = json.loads(response.text)
     token = response_body["response"]["token"] # token is used to get information
-
+    print("--------------------------------------------------")
     print("Welcome to the expense tracking app!")
-    print("Here are the current categories of your expenses")
+    print("--------------------------------------------------")
+    print("Here are the current categories of your expenses, along with key words that identify transactions to be in that category")
+    print("--------------------------------------------------")
     for c in categories:
-        print(c.title())
+        print(c.title()+":")
+        print(return_list("db/"+c+".csv"))
+    print("--------------------------------------------------")
     print()
     account_url =  base + "/accounts?token=" + token #url to get account(s)
     account = show_account(account_url)
-    print()
-    print("Here is a list of your transactions:")
     transactions_url =  base + "/transactions?token=" + token #url to get transactions
     transactions = list_transactions(transactions_url) #a list of all the transactions
-    print()
     listed_data = [] #this is a list of all the transactions (which are each a dictionary)
     for t in transactions:
         listed_data.append({"Date":t["normalizedDate"],"Description":t["description"],"Amount":-t["expenseAmount"]})
+    while True:
+        print(f"""
+        Please select from the following:
+            Operation | Description
+            --------- | ------------------
+            Category  | Adds a category to sort your expenses by
+            Key Word  | Adds a key word to a specific category
+            ?Budget   | Sets a budget for a category
+            Summarize | Summarizes your expenses
+            """)
+        action = input("Please select an operation: ").title()
+        if(action == "Summarize"):
+            summarize(categories,transactions)
 
+
+def summarize(categories, transactions):
     #this creates a list of lists of key words for each category.
     #category_key_words[0] would be key words for the first category and so on so forth.
     category_key_words = []
@@ -78,73 +101,42 @@ def run():
     #this creates the list of list of dictionary transactions for each category
     category_payments = []
     for c in categories:
-        category_payments.append(include_transactions(transactions,category_key_words[categories.index(c)]))
-    for c in category_payments:
-        for i in c:
-            print(i)
+        category_payments.append(include_transactions(transactions,category_key_words[categories.index(c)])) #cycles through key words for that category
 
-def include_transactions(transactions,key_words):
-    included_transactions = []
+    # for everything else, the transactions is grouped to "other"
     for t in transactions:
-        for f in key_words:
-            if(f.upper() in t["description"].upper()):
-                included_transactions.append(t)
-    return included_transactions
-    #print("You spend a total of "+str(food_total)+" on food. Here is a list of the transactions")
-    #for i in food_payments:
-    #    print("Date: "+i["normalizedDate"]+" Description: "+i["description"]+" Amount: "+str(i["amount"]))
+        add_other = True
+        for c in category_payments:
+            if(t in c):
+                add_other = False
+        if(add_other):
+            category_payments[1].append(t)
 
+    count = -1
+    for c in reversed(category_payments):
+        print("---------")
+        print(categories[count].title()+":")
+        print("---------")
+        sum=0
+        for i in c:
+            sum = sum-i["expenseAmount"]
+            print(i["normalizedDate"]+": "+i["description"]+": "+"(${0:.2f})".format(float((-i["expenseAmount"]))))
+        print("---------")
 
-    ##sum of each category
-#food_total=0
-#expense_total=0
-#shopping_total=0
-#other_total=0
-#income_total=0
-
-
-    #for t in transactions:
-    #    for f in expense_key_words:
-    #        if(f.upper() in t["description"].upper()):
-    #            expense_payments.append(t)
-    #            expense_total= expense_total+t["amount"]
-    #            #transactions.remove(t) this will screw it up. find a way to remove.
-    #            break
-    #print("You spend a total of "+str(expense_total)+" on expense. Here is a list of the transactions")
-    #for i in expense_payments:
-    #    print("Date: "+i["normalizedDate"]+" Description: "+i["description"]+" Amount: "+str(i["amount"]))
-#
-    #for t in transactions:
-    #    for f in shopping_key_words:
-    #        if(f.upper() in t["description"].upper()):
-    #            shopping_payments.append(t)
-    #            shopping_total= shopping_total+t["amount"]
-    #            #transactions.remove(t) this will screw it up. find a way to remove.
-    #            break
-    #print("You spend a total of "+str(shopping_total)+" on shopping. Here is a list of the transactions")
-    #for i in shopping_payments:
-    #    print("Date: "+i["normalizedDate"]+" Description: "+i["description"]+" Amount: "+str(i["amount"]))
-#
-    #for t in transactions:
-    #    for f in other_key_words:
-    #        if(f.upper() in t["description"].upper()):
-    #            other_payments.append(t)
-    #            other_total= other_total+t["amount"]
-    #            #transactions.remove(t) this will screw it up. find a way to remove.
-    #            break
-    #print("You spend a total of "+str(other_total)+" on other. Here is a list of the transactions")
-    #for i in other_payments:
-    #    print("Date: "+i["normalizedDate"]+" Description: "+i["description"]+" Amount: "+str(i["amount"]))
-#
-#
-    #for t in transactions:
-    #    if -t["expenseAmount"]>0:
-    #        income.append(t)
-    #        #transactions.remove(t)
-    #print("Your income/refund items are shown below")
-    #for i in income:
-    #    print("Date: "+i["normalizedDate"]+" Description: "+i["description"]+" Amount: "+str(i["amount"]))
-
+        if(sum<0):
+            print("The total amount spend on "+ categories[count].title() + " items is " +"(${0:.2f})".format(float(sum)))
+            if(sum<-100):
+                print("You are spending way too much in this category! Cut down!")
+            else:
+                print("You are thrifty when it comes to "+categories[count]+". Feel free to spend more =]")
+            print()
+        elif sum==0:
+            print("You have no activity in the "+categories[count].title()+" category")
+            print()
+        else:
+            print("Your income/refunds for the month is "+ "(${0:.2f})".format(float(sum)))
+            print()
+        count+=-1
 
 
 if __name__ == "__main__": ## this will run only when this program is invoked from the command line. So if we run the reset function, it wont run the whole thing
