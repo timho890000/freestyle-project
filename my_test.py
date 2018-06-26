fd74d880815ef7b5fd8aac05e9bc1da4eddd3ef0
timho890000 <39267632+timho890000@users.noreply.github.com>
2018-06-25 22:19:18 -0400

    Added some more tests.

--- my_test.py
+++ my_test.py
@@ -1,11 +1,7 @@
 # from freestyle-project.expense-tracker import
 import sys
 import os
-from expense_tracker import show_account
-from expense_tracker import list_transactions
-
-
-
+from app import *
 
 def test_show_account():
     result = show_account("https://www.buxfer.com/api/accounts?token=kq5uhi7qksv6k8uo7503hu3jh6")
@@ -15,6 +11,15 @@ def test_list_transactions():
     result = list_transactions("https://www.buxfer.com/api/transactions?token=kq5uhi7qksv6k8uo7503hu3jh6&startDate=2018-05-01&endDate=2018-05-30")
     assert type(result) == list
 
+def test_include_category_key_words():
+    result = include_category_key_words("categories")
+    assert type(result) == list
 
-if __name__ == "__main__": # "if this script is run from the command-line, then ..."
-    test_show_account()
+def test_return_list():
+    result = return_list("db/categories.csv")
+    assert type(result) == list
+
+#not sure how to test a function with inputs
+#def test_set_budget():
+#    result = set_budget(["food","drinks"],["100","100"])
+#    assert type(result) == list
