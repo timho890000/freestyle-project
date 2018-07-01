f4da984b72a3ecc9ce8b06eab0f41573a6881f71
timho890000 <39267632+timho890000@users.noreply.github.com>
2018-07-01 18:05:08 -0400

    set environment variables for security purposes

--- my_test.py
+++ my_test.py
@@ -1,11 +1,34 @@
 # from freestyle-project.expense-tracker import
 import sys
 import os
+from dotenv import load_dotenv
 from app import *
 
+load_dotenv()
+
+username = os.environ.get("BUXFER_USERNAME") or "OOPS. Please set an environment variable named 'BUXFER_USERNAME'."
+if username == "OOPS. Please set an environment variable named 'BUXFER_USERNAME'.":
+    print(username)
+    exit()
+password = os.environ.get("BUXFER_PASSWORD") or "OOPS. Please set an environment variable named 'BUXFER_PASSWORD'."
+if password == "OOPS. Please set an environment variable named 'BUXFER_PASSWORD'.":
+    print(password)
+    exit()
+username = username.replace('"', '') #removes the quotations from the environment variables
+password = password.replace('"', '')
+print(username)
+print(password)
+
+base = "https://www.buxfer.com/api";
+login_url  = base + "/login?userid=" + username + "&password=" + password
+response = requests.get(login_url)
+response_body = json.loads(response.text)
+token = response_body["response"]["token"] # token is used to get information
+
 def test_show_account():
-    result = show_account("https://www.buxfer.com/api/accounts?token=kq5uhi7qksv6k8uo7503hu3jh6")
+    result = show_account(base + "/accounts?token=" + token)
     assert type(result) == dict
+
 
 def test_list_transactions():
     result = list_transactions("https://www.buxfer.com/api/transactions?token=kq5uhi7qksv6k8uo7503hu3jh6&startDate=2018-05-01&endDate=2018-05-30")
